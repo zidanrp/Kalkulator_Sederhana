c0a99ebdcfb15f409d848d9ebde61bcc02b341f0
zidanrp <prdozrp@gmail.com>
2024-03-18 15:47:22 +0700

    v1.0

---
+++ kalkulator.js
@@ -0,0 +1,28 @@
+function hitung() {
+    var angka1 = parseFloat(document.getElementById("angka1").value);
+    var angka2 = parseFloat(document.getElementById("angka2").value);
+    var operator = document.getElementById("operator").value;
+    var hasil;
+
+    switch (operator) {
+        case "tambah":
+            hasil = angka1 + angka2;
+            break;
+        // case "kurang":
+        //     hasil = angka1 - angka2;
+        //     break;
+        // case "kali":
+        //     hasil = angka1 * angka2;
+        //     break;
+        // case "bagi":
+        //     hasil = angka1 / angka2;
+        //     break;
+        // case "modulus":
+        //     hasil = angka1 % angka2;
+        //     break;
+        // default:
+        //     hasil = "Operasi tidak valid";
+    }
+
+    document.getElementById("hasil").innerText = "Hasil: " + hasil;
+}
